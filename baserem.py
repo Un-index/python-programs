ce99224b395819fa27bd9817cfdb4fa7a76daf8a
Un-index <68165727+Un-index@users.noreply.github.com>
2020-10-08 13:34:13 +0300

    Create baserem.py

---
+++ baserem.py
@@ -0,0 +1,17 @@
+
+
+n, base = eval(input("enter number and base here, separated by a comma(,): "));
+basestr = str(base)+"|"
+print(f"{basestr} {n} | r")
+print("_________")
+maxdgts = len(str(n))+1;
+remchain = ""
+
+while(n>0):
+    rem = n%base
+    remchain += str(int(rem))
+    n = (n-rem)/base
+    currdgts = len(str(n))
+    print(basestr+f" {int(n)}{(maxdgts-currdgts + 2)*' '}| {int(rem)}")
+
+print("(reversed remainders) binary:", remchain[::-1])
