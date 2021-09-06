646a799d4a92d640690a96f4a1af8b5f62279ae4
Un-index <68165727+Un-index@users.noreply.github.com>
2021-09-06 19:59:43 +0300

    Update and rename base-rem.py to decimal to binary.py

--- decimal to binary.py
+++ decimal to binary.py
@@ -1,5 +1,7 @@
 
 
+# include base specification feature to serve to illustrate how this would work with other bases
+
 n, base = eval(input("enter number and base here, separated by a comma(,): "));
 basestr = str(base)+"|"
 print(f"{basestr} {n} | r")
